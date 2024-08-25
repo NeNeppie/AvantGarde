5c6fc00d1a4a3a6d37f8cdb5df74e07fb66ade5b
NeNeppie <108415463+NeNeppie@users.noreply.github.com>
2024-08-26 00:02:33 +0300

    Version Bump `0.1.1.2`

--- AvantGarde/Data/DataManager.cs
+++ AvantGarde/Data/DataManager.cs
@@ -5,6 +5,27 @@ using System.Threading.Tasks;
 
 namespace AvantGarde.Data;
 
+/*
+TODO: Structure of FashionCheck AtkValues (as of Endwalker), 
+    for future automatic submission implementation:
+    [0] Weekly Theme
+    [1] Sub-struct for every gear piece
+        [0] Bool - Hint active
+        [1] String - Hint
+------> [2] UInt - Score stamp (0 is Gold, 4 is Low, 5 is None)
+        [3] UInt - unk
+        [4] String - Slot
+        [5] UInt - unk
+        [6] UInt - unk
+------> [7] UInt - Dye ID
+        [8] UInt - unk (Connected to dye)
+        [9] Bool - unk
+        [10] Bool - unk
+    [122] Remaining Attempts
+    [123] High Score
+    [124-132] Irrelevant
+*/
+
 public class DataManager
 {
     public Dictionary<uint, List<int>> Data = new();
