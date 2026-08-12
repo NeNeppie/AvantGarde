9261d9e9a550ff0cabbd23587f65aa029ae93dad
NeNeppie <108415463+NeNeppie@users.noreply.github.com>
2026-08-12 13:39:45 +0300

    Reword info window for stable release

--- AvantGarde/UI/DataCollectionWindow.cs
+++ AvantGarde/UI/DataCollectionWindow.cs
@@ -8,6 +8,8 @@ namespace AvantGarde.UI;
 
 public class DataCollectionWindow
 {
+    private static readonly Vector4 ColorDiscordBlurple = new(0.34f, 0.40f, 0.95f, 1.0f);
+    private static readonly Vector4 ColorGithubOrange = new(0.50f, 0.12f, 0.06f, 1.0f);
     private static readonly ImGuiWindowFlags WindowFlags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.AlwaysAutoResize;
     private bool _shouldDraw = true;
     private bool _doNotShowAgain = false;
@@ -27,16 +29,7 @@ public class DataCollectionWindow
 
         using (var style = ImRaii.PushStyle(ImGuiStyleVar.ItemSpacing, new Vector2(ImGui.GetStyle().ItemSpacing.X, 10f)))
         {
-
             ImGui.TextColored(new Vector4(0.2f, 0.52f, 0.83f, 1f), "Thank you for using Avant-Garde!");
-            ImGui.Separator();
-
-            ImGui.TextWrapped("""
-            You're now testing the new automatic crowdsourcing feature.
-            As the database is being migrated, you might find that some categories are empty or lacking items.
-            If you wish to see the full list of items, switch back to the stable version.
-            If you encounter any issues, please report them on Dalamud's Discord, in either the plugin testing channel or Avant-Garde's forum post.
-            """);
             ImGui.Separator();
         }
 
@@ -54,6 +47,9 @@ public class DataCollectionWindow
         ImGui.Spacing();
 
         ImGui.Text("You can always change your mind by ticking the box inside the Fashion Report window.\nFunctionality is not affected by your choice.");
+        ImGui.Spacing();
+
+        ImGui.TextWrapped("If you encounter any bugs or have any suggestions, please open an issue on github, or let me know on Dalamud's Discord.");
 
         ImGui.Spacing();
         ImGui.Separator();
@@ -70,9 +66,14 @@ public class DataCollectionWindow
         }
         ImGui.SameLine();
 
-        using (var color = ImRaii.PushColor(ImGuiCol.Button, new Vector4(0.345f, 0.396f, 0.949f, 1.0f)))
+        using (var color = ImRaii.PushColor(ImGuiCol.Button, ColorDiscordBlurple))
         {
             GuiUtilities.HyperlinkButton("Discord Forum Post", "https://discord.com/channels/581875019861328007/1166794253553381456");
+            ImGui.SameLine();
+        }
+        using (var color = ImRaii.PushColor(ImGuiCol.Button, ColorGithubOrange))
+        {
+            GuiUtilities.HyperlinkButton("Github Issues", "https://github.com/NeNeppie/AvantGarde/issues");
         }
 
         ImGui.End();
